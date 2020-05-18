b5b0f996e7e02e0926f7550ea85b4753e9179983
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-18 06:21:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,16 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  bbq_days = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |day, supplies|
+      if supplies.include?("BBQ")
+        bbq_days << day
+      end
+    end
+  end
+  bbq_days
 
 end
 
