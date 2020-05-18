4776e84f74a24782a4568f915bb1d9aa557ed223
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-18 05:51:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season, holiday_hash|
-    puts "#{season.upcase}" << ":"
+    puts "#{season.capitalize}" << ":"
   end
 
 end
